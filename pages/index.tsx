507900c0e295bab5a5408cd8e98fbaffdda2ccf8
Patricio López Juri <lopezjuripatricio@gmail.com>
2020-12-05 18:55:04 -0300

    Comment lines to make it work

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,15 @@ import { Button, Logo } from '../components'
 
 const App = () => (
   <div
-    css={[
-      tw`flex flex-col items-center justify-center h-screen`,
-      tw`bg-gradient-to-b from-electric to-ribbon`
-    ]}
+    // css={[
+    //   tw`flex flex-col items-center justify-center h-screen`,
+    //   tw`bg-gradient-to-b from-electric to-ribbon`
+    // ]}
   >
-    <div tw="flex flex-col justify-center h-full space-y-5 ">
+    <div
+      // 👇👇👇 Uncomment this line to break the app
+      // tw="flex flex-col justify-center h-full space-y-5 "
+      >
       <Button isPrimary>Submit</Button>
       <Button isSecondary>Cancel</Button>
       <Button isSmall>Close</Button>
